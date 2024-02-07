b815f1ae8d4ba2ad1581064f8f6fa7e869643de1
seonb2n <626ksb@gmail.com>
2024-02-07 20:17:59 +0900

    refactoring: ErrorResponse 가 반환되는 경우, CommonResponse 의 필드로서 반환

--- src/main/java/com/example/springboard/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/springboard/common/handler/GlobalExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.example.springboard.common.handler;
 
 import com.example.springboard.common.exception.GlobalException;
+import com.example.springboard.dto.response.CommonResponse;
 import com.example.springboard.dto.response.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,14 +15,15 @@ public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(GlobalException.class)
-    public ResponseEntity<ErrorResponse> handleException(GlobalException exception) {
+    public CommonResponse<ErrorResponse> handleException(GlobalException exception) {
         // todo logger 처리
         ErrorResponse errorResponse = new ErrorResponse(exception.getErrorType());
         if (exception.getErrorType().getCode() >= 500_000) {
-            return ResponseEntity.internalServerError().body(errorResponse);
+            return CommonResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                errorResponse);
         } else if (exception.getErrorType().getCode() >= 400_000) {
-            return ResponseEntity.badRequest().body(errorResponse);
+            return CommonResponse.of(HttpStatus.BAD_REQUEST.getReasonPhrase(), errorResponse);
         }
-        return ResponseEntity.badRequest().body(errorResponse);
+        return CommonResponse.of(HttpStatus.BAD_REQUEST.getReasonPhrase(), errorResponse);
     }
 }
